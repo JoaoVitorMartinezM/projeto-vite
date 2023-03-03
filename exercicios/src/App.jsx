b5a7f5fcd1b89e7cc68a974eb529dafd16e3ed45
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-03-03 20:45:21 -0300

    footer

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -2,12 +2,14 @@
 // import './App.css'
 import PropTypes from 'prop-types';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
       
     </div>
   )
---
+++ exercicios/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => { 
+    return(
+        <div className="Footer">
+            <span>React Cursos © by JoaoVitorMartinezM </span>
+        </div>
+    );
+}
+
+export default Footer;
