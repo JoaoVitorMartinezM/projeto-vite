a99d067394d0befcbb6696042c9101f2b3e3d406
Joao Vitor Martinez <justdatascientist@gmail.com>
2023-03-07 18:24:00 -0300

    Filtro concluído

--- exercicios/src/App.jsx
+++ exercicios/src/App.jsx
@@ -3,82 +3,15 @@
 import PropTypes from 'prop-types';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Card from './components/Card';
-import ListCourses from './components/ListCourses';
+import { lista } from './lista';
+import Filter from './components/Filter';
 
 function App() {
-
-  const cardList = [
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    },
-    {
-      name:"Teste",
-      category:"Front-End",
-      duration:"8h",
-      description:"Uma descrição para o curso de React",
-      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
-  
-    }
-]
 
   return (
     <div className="App">
       <Navbar/>
-      <ListCourses list={cardList}/>
+      <Filter/>
       <Footer/>
       
     </div>
